7904c8e887e38e341f4d3dd0ebb45dbaade50607
Nataly-Wambui <wambuinatalygakenye@gmail.com>
2024-02-07 12:29:10 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+  var fullName: String = "Nataly Glory"
+    println(fullName)
+
+    var age: Byte = 21
+    println(age)
+
+    var phoneNumber: Long = 7000000000
+    println(phoneNumber)
+
+    var weight: Float = 45.1F
+    println(weight)
+
+    var kenyan: Boolean = true
+    println(true)
+
+    var nonKenyan: Boolean = false
+    println(false)
+
+
+
+
+}
